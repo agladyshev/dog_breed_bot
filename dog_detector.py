3634a6e2591f362449991b73c6f96ec8f54ff226
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2018-04-05 19:16:30 +0300

    refactor: Move dog detetor to a standalone class

--- dog_detector.py
+++ dog_detector.py
@@ -1,22 +1,40 @@
 from keras.applications.resnet50 import ResNet50
-from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
 from keras.applications.resnet50 import preprocess_input, decode_predictions
-from helpers import image_to_tensor
+import numpy as np
+import tensorflow as tf
 
-import logging
 
 class DogDetector:
     def __init__(self):
-        self.ResNet50_model = ResNet50(weights='imagenet')
-        logging.info('Initialize dog detector')
+        # global ResNet50_model
+        self.ResNet50_model = ResNet50(weights="imagenet")
+        # global graph
+        self.graph = tf.get_default_graph()
+        print('load model')
 
-    def ResNet50_predict_labels(image):
+    def prepare_image(self, image, target):
+
+        # if the image mode is not RGB, convert it
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+
+        # resize the input image and preprocess it
+        image = image.resize(target)
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+
+        # return the processed image
+        return image
+
+    def ResNet50_predict_labels(self, image):
         # returns prediction vector for image located at img_path
-        img = preprocess_input(image_to_tensor(image))
+        img = self.prepare_image(image, target=(224, 224))
         return np.argmax(self.ResNet50_model.predict(img))
 
     ### returns "True" if a dog is detected in the image stored at img_path
-    def detect(image):
-        logging.info('process image')
-        prediction = ResNet50_predict_labels(image)
+    def detect(self, image):
+        with self.graph.as_default():
+            prediction = self.ResNet50_predict_labels(image)
         return ((prediction <= 268) & (prediction >= 151))
--- main.py
+++ main.py
@@ -5,60 +5,9 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, BaseFilter, Fi
 import logging
 import re
 import json
-
-
-
-from keras.applications.resnet50 import ResNet50
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
-from PIL import Image
-from keras.applications.resnet50 import preprocess_input, decode_predictions
-import numpy as np
-import tensorflow as tf
 import io
-
-
-def load_model():
-    # load the pre-trained Keras model (here we are using a model
-    # pre-trained on ImageNet and provided by Keras, but you can
-    # substitute in your own networks just as easily)
-    global ResNet50_model
-    ResNet50_model = ResNet50(weights="imagenet")
-    global graph
-    graph = tf.get_default_graph()
-    print('load model')
-
-
-def prepare_image(image, target):
-
-    # if the image mode is not RGB, convert it
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-
-    # resize the input image and preprocess it
-    image = image.resize(target)
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    # image = imagenet_utils.preprocess_input(image)
-
-    # return the processed image
-    return image
-
-def ResNet50_predict_labels(image):
-    # returns prediction vector for image located at img_path
-    img = prepare_image(image, target=(224, 224))
-    return np.argmax(ResNet50_model.predict(img))
-
-### returns "True" if a dog is detected in the image stored at img_path
-def detect(image):
-    prediction = ResNet50_predict_labels(image)
-    print(prediction)
-    return ((prediction <= 268) & (prediction >= 151))
-
-
-
-
-
+from PIL import Image
+from dog_detector import DogDetector
 
 # updater = Updater(token=os.environ.get('TOKEN'))
 updater = Updater(token='579059922:AAHyb14wI8jf6Qxgx-4QqOyCKj6O_Xw2osQ')
@@ -67,7 +16,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
-# dogDetector = DogDetector()
+dogDetector = DogDetector()
 
 def whatBreed(bot, update):
 
@@ -75,10 +24,11 @@ def whatBreed(bot, update):
     image = open('input.jpg', 'rb')
     image = image.read()
     image = Image.open(io.BytesIO(image))
-
-    global graph
-    with graph.as_default():
-        is_dog = detect(image)
+    # try:
+    #   is_dog = dogDetector.detect(image=image)
+    # except:
+    #   sys.exit(1)
+    is_dog = dogDetector.detect(image=image)
     print(is_dog)
   
 
@@ -90,7 +40,6 @@ def whatBreed(bot, update):
 handler = MessageHandler(Filters.photo, 
                          whatBreed)
 
-load_model()
 
 dispatcher = updater.dispatcher
 
