306e770bda9c3b682cfabca60eab6d65df2e5765
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2018-04-05 15:45:06 +0300

    feat: switch to dog breed classification, respond to image sent

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
 import os
-from telegram.ext import Updater, CommandHandler, MessageHandler, BaseFilter
+from telegram.ext import Updater, CommandHandler, MessageHandler, BaseFilter, Filters
 import logging
 import re
-import pendulum
-from datetime import datetime
 import json
 
 
-updater = Updater(token=os.environ.get('TOKEN'))
-
+# updater = Updater(token=os.environ.get('TOKEN'))
+updater = Updater(token='579059922:AAHyb14wI8jf6Qxgx-4QqOyCKj6O_Xw2osQ')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
-
-class FilterIgor(BaseFilter):
-
-    def filter(self, message):
-        if message.text:
-            return re.search(u'Игор[^\s{1, 3}]', message.text, re.UNICODE)
-
-filter_igor = FilterIgor()
-
-
-def whatTime(bot, update):
-    khabarovskh_time = "В Комсомольске-на-Амуре " + \
-        pendulum.now('Asia/Vladivostok').format('%H:%M')
+def whatBreed(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
-                     text=khabarovskh_time)
+                     text='got it')
+    
+handler = MessageHandler(Filters.photo, 
+                         whatBreed)
 
 
-time_handler = MessageHandler(filter_igor, whatTime)
 
 
 dispatcher = updater.dispatcher
-dispatcher.add_handler(time_handler)
+
+dispatcher.add_handler(handler)
 
 
 updater.start_polling()
